50755069ff373ab511c1efd0710426e02c95291c
mscalfi <marco.scalfi@protonmail.com>
2022-02-28 19:06:41 +0100

    Initial Spring setup

---
+++ src/main/java/it/ms/backendassignment/BackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package it.ms.backendassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignmentApplication.class, args);
+	}
+
+}
