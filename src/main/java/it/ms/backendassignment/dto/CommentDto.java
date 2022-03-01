a3ffa3b0b8e9c99312495758b31dd98ac401c641
mscalfi <marco.scalfi@protonmail.com>
2022-03-01 22:24:06 +0100

    Removed accidental Service notation form CommentDto

--- src/main/java/it/ms/backendassignment/dto/CommentDto.java
+++ src/main/java/it/ms/backendassignment/dto/CommentDto.java
@@ -1,10 +1,8 @@
 package it.ms.backendassignment.dto;
 
 import lombok.Data;
-import org.springframework.stereotype.Service;
 
 @Data
-@Service
 public class CommentDto {
     private Long postId;
     private String text;
