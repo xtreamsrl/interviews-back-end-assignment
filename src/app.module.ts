122973f8a18bc3c6587cd637b5dc0fa755f1764d
Riccardo Lin <chenweiriccardo.lin@gmail.com>
2024-03-20 22:49:11 +0100

    feat: add new import to project and inject it on appModule + initialize db connection and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger(); // initialize nest logger
+  let port;
+
+  const env = process.env.NODE_ENV;
+  const http = env === 'prod' ? 'https' : 'http';
+  const DOMAIN = env === 'dev' && 'localhost';
+
+  console.log(port);
+  if (process.env.NODE_ENV === 'dev') {
+    port = process.env.PORT_DEV;
+  } else if (process.env.NODE_ENV === 'prod') {
+    port = process.env.PORT_PROD; //currently there is no production environment
+  }
+  const DNS = `${http}://${DOMAIN}:${port}`;
+
+  await app.listen(port);
+  logger.log(`server started on port ${DNS} (${env})`);
 }
 bootstrap();
