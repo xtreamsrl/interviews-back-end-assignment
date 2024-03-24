aa7e3ea977ca06700400af9fbeabf27581f50df9
Riccardo Lin <chenweiriccardo.lin@gmail.com>
2024-03-24 22:52:54 +0100

    feat: simple authentication with jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +18,8 @@ import { CategoriesModule } from './categories/categories.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     ProductsModule,
     CategoriesModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
